01a991327c7bd093ad860b8736e074a3d8901f10
mafalu0606 <1974794628@qq.com>
2019-06-09 19:57:19 +0800

    diyi

--- app/src/main/java/com/bawei/myapplication/MainActivity.java
+++ app/src/main/java/com/bawei/myapplication/MainActivity.java
@@ -3,6 +3,7 @@ package com.bawei.myapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +13,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //按钮
         Button button = new Button(this, null);
+        TextView textView = new TextView(this, null);
     }
 }
